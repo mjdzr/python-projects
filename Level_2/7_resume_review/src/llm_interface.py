e3976ecfa2a6c966f46857495f59eb21343dcb0a
mjdzr <majid.zare@ualberta.ca>
2025-07-17 23:48:42 -0600

    llm code added to resume review project

---
+++ Level_2/7_resume_review/src/llm_interface.py
@@ -0,0 +1,25 @@
+import os
+
+from dotenv import find_dotenv, load_dotenv
+from openai import OpenAI
+
+# Find and load the .env file
+load_dotenv(find_dotenv())
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def call_llm(prompt, model = "gpt-4.1"):
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant that extracts information from resumes."
+            },
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+    return response.choices[0].message.content
